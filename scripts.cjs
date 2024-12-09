2bf3a6ac74e927f556b411870c8b4dc92202ac60
Ani <intrepidani05@gmail.com>
2024-12-09 15:19:00 +0530

    Fix typos in view settings and improve static file path in scripts; update EJS stylesheet link in index.ejs

--- scripts.cjs
+++ scripts.cjs
@@ -5,26 +5,28 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Listing = require('./models/Listing');
 
-app.set("veiews", path.join(__dirname, "/views"))
+app.set("views", path.join(__dirname, "/views"))
 app.set("view engine", "ejs");
-function ConnectDB(){
+app.use(express.static(path.join(__dirname, "/public")));
+
+function ConnectDB() {
     mongoose.connect('mongodb://localhost:27017/WonderLust')
-    .then(() => {
-        console.log("Database connected successfully");
-    })
-    .catch((err) => {
-        console.log("ERROR: ", err);
-    });
-} 
+        .then(() => {
+            console.log("Database connected successfully");
+        })
+        .catch((err) => {
+            console.log("ERROR: ", err);
+        });
+}
 ConnectDB();
 
 
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
     const listings = await Listing.find();
-    res.render("index", {listings});
+    res.render("index", { listings });
 });
 
-app. listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}!`);
 });
 
