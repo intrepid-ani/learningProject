8454e162771c9df6673959e157134237ae9e891a
Ani <intrepidani05@gmail.com>
2024-12-09 14:45:47 +0530

    Update DB connection and logic

--- init/index.js
+++ init/index.js
@@ -4,15 +4,27 @@ const Listing = require('../models/Listing');
 let data = require('./data');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/WonderLust', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-Listing.insertMany(data)
-    .then((res) => {
-        console.log("Data inserted successfully");
+// Connecting db
+function ConnectDB(){
+    mongoose.connect('mongodb://localhost:27017/WonderLust')
+    .then(() => {
+        console.log("Database connected successfully");
     })
     .catch((err) => {
         console.log("ERROR: ", err);
     });
+}
+
+ConnectDB();
+
+// Inserting data
+async function insert () {
+    try {
+        await Listing.insertMany(data);
+        console.log("Data inserted successfully");
+    } catch (err) {
+        console.log("ERROR: ", err);
+    }
+}
+
+insert();
