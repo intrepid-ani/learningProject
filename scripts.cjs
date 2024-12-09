0154175c1faa80afb3f95e662d1fc9f1ab4a8816
Ani <intrepidani05@gmail.com>
2024-12-09 15:07:33 +0530

    Implement database connection and update homepage to display listings

--- scripts.cjs
+++ scripts.cjs
@@ -2,12 +2,26 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const path = require('path');
+const mongoose = require('mongoose');
+const Listing = require('./models/Listing');
 
-app.set("veiews", path.join(__dirname, "/veiws"))
+app.set("veiews", path.join(__dirname, "/views"))
 app.set("view engine", "ejs");
+function ConnectDB(){
+    mongoose.connect('mongodb://localhost:27017/WonderLust')
+    .then(() => {
+        console.log("Database connected successfully");
+    })
+    .catch((err) => {
+        console.log("ERROR: ", err);
+    });
+} 
+ConnectDB();
 
-app.get("/", (req, res) => {
-    res.render("index");
+
+app.get("/", async(req, res) => {
+    const listings = await Listing.find();
+    res.render("index", {listings});
 });
 
 app. listen(port, () => {
