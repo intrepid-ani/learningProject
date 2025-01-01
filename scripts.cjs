3fb4b4b5bae421ae3a7de11015eb1388f2fd3450
Ani <intrepidani05@gmail.com>
2025-01-01 18:46:36 +0530

    Implement listing creation functionality and update index view with a new listing button

--- scripts.cjs
+++ scripts.cjs
@@ -3,11 +3,13 @@ const app = express();
 const port = 8000;
 const path = require('path');
 const mongoose = require('mongoose');
-const Listing = require('./models/Listing');
+const Listing = require('./models/listing');
+// const List = require('./models/list');
 
 app.set("views", path.join(__dirname, "/views"))
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
 
 function ConnectDB() {
     mongoose.connect('mongodb://localhost:27017/WonderLust')
@@ -38,6 +40,39 @@ app.get("/detail/:id", async (req, res) => {
     } catch (err) {
         console.error("Error fetching listing", err);
         res.status(500).send("Internal Server Error");
+    }
+});
+
+app.get("/create-list",(req, res) => {
+    res.render("list");
+});
+
+// app.post("/create-list", async (req, res) => {
+//     const { title, description, price, location } = req.body;
+//     try {
+//         const newListing = new Listing({ title, description, price, location });
+//         await newListing.save();
+//         res.redirect("/list");
+//     } catch (err) {
+//         console.error("Error creating listing", err);
+//         res.status(500).send("Internal Server Error");
+//     }
+// });
+
+app.post('/create-list', async (req, res) => {
+    try {
+        await Listing.create({
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price,
+            location: req.body.location,
+            country: req.body.country
+        });
+        
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error creating new list:', error);
+        res.status(500).send('Error creating new listing');
     }
 });
 
